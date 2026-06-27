e81dc21da01de815d76b05b88b148424dad87f48
Hikari <108615941+GoodLight999@users.noreply.github.com>
2026-06-27 20:55:28 +0900

    fix(android): allow enough time for headless recovery

--- ClipCascade_Mobile/src/android/app/src/main/java/com/clipcascade/HeadlessTaskService.kt
+++ ClipCascade_Mobile/src/android/app/src/main/java/com/clipcascade/HeadlessTaskService.kt
@@ -1,4 +1,3 @@
-// android\app\src\main\java\com\clipcascade\HeadlessTaskService.kt
 package com.clipcascade
 
 import android.content.Intent
@@ -6,17 +5,14 @@ import com.facebook.react.HeadlessJsTaskService
 import com.facebook.react.bridge.Arguments
 import com.facebook.react.jstasks.HeadlessJsTaskConfig
 
-
 class HeadlessTaskService : HeadlessJsTaskService() {
-
-    override fun getTaskConfig(intent: Intent?): HeadlessJsTaskConfig? {
-        return intent?.extras?.let {
+    override fun getTaskConfig(intent: Intent?): HeadlessJsTaskConfig? =
+        intent?.extras?.let { extras ->
             HeadlessJsTaskConfig(
-                "Restart", // JS task name
-                Arguments.fromBundle(it), // Data passed to the task
-                5000, // Timeout for the task
-                true // Allow task to run in foreground
+                "Restart",
+                Arguments.fromBundle(extras),
+                30_000,
+                true,
             )
         }
-    } 
 }
