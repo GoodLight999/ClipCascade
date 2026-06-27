ed42333ec46cf236c52e33f14b13bc36cf555479
Hikari <108615941+GoodLight999@users.noreply.github.com>
2026-06-27 20:55:43 +0900

    fix(android): acquire wake lock for boot recovery task

--- ClipCascade_Mobile/src/android/app/src/main/java/com/clipcascade/BootReceiver.kt
+++ ClipCascade_Mobile/src/android/app/src/main/java/com/clipcascade/BootReceiver.kt
@@ -1,21 +1,18 @@
-// android\app\src\main\java\com\clipcascade\BootReceiver.kt
 package com.clipcascade
 
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.facebook.react.HeadlessJsTaskService
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
-            startHeadlessTask(context)
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            val headlessTaskIntent = Intent(context, HeadlessTaskService::class.java).apply {
+                putExtra("event", "BOOT_COMPLETED")
+            }
+            context.startService(headlessTaskIntent)
+            HeadlessJsTaskService.acquireWakeLockNow(context)
         }
-    }
-
-    private fun startHeadlessTask(context: Context) {
-        val headlessTaskIntent = Intent(context, HeadlessTaskService::class.java).apply {
-            putExtra("event", "BOOT_COMPLETED")
-        }
-        context.startService(headlessTaskIntent)
     }
 }
